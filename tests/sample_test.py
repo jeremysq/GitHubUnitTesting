ae6692769db71483bbd5f3a0588f28d81925955c
jeremysq <jeremyx77@gmail.com>
2020-05-05 21:46:26 -0400

    Testing

---
+++ tests/sample_test.py
@@ -0,0 +1,6 @@
+def test_example():
+    """
+    But really, test cases should be callables containing assertions:
+    """
+    print("\nRunning test_example...")
+    return "This test_example function ran."
