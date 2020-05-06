9b054da1c199998763fe4a9691584bbc12e354dd
jeremysq <jeremyx77@gmail.com>
2020-05-05 21:49:04 -0400

    Testing

--- tests/sample_test.py
+++ tests/sample_test.py
@@ -1,6 +1,9 @@
+def func(x):
+    return x + 1
+
 def test_example():
     """
     But really, test cases should be callables containing assertions:
     """
     print("\nRunning test_example...")
-    return "This test_example function ran."
+    assert func(2)
