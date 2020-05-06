6369aa3df44cfb8607c3b49067dcf8a8fb8f6089
jeremysq <jeremyx77@gmail.com>
2020-05-05 23:21:23 -0400

    Using davidism example for testing

--- tests/sample_test.py
+++
@@ -1,8 +0,0 @@
-from app import app
-import requests
-
-app.run(debug=True,host="0.0.0.0")
-
-def test_example():
-    x = requests.get('http://localhost:5000/shutdown')
-    assert x == "Server shutting down..."
